7073ffcf20a5eeb3a9d26eb16e1221d02ddf8c5e
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-09-12 23:47:30 +0200

    :wrench: Create tailwind.config.js.

---
+++ nodejs/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["../public/**/*.php","../public/*.php","./src/**/*.ts","../src/**/*.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
