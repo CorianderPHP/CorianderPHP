f05ab73c46398421b695693b6ca8e1d8d574e49f
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-09-13 13:23:52 +0200

    :speech_balloon: Create home metadata.

---
+++ public/public_views/home/metadata.php
@@ -0,0 +1,5 @@
+<?php
+// Custom metadata for the Home page
+$lang = 'en';
+$metaTitle = 'Welcome to CorianderPHP';
+$metaDescription = 'Welcome to CorianderPHP, a lightweight and performance-focused PHP framework.';
