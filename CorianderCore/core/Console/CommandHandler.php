94dc80b11cde827505e0f4fdd6a35e3a528b02fb
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-09-13 17:41:09 +0200

    :bulb: Update comments.

--- CorianderCore/core/Console/CommandHandler.php
+++ CorianderCore/core/Console/CommandHandler.php
@@ -7,6 +7,9 @@ class CommandHandler
     /**
      * Available commands and their corresponding handler classes.
      *
+     * An associative array mapping command names (e.g., 'hello', 'nodejs')
+     * to their respective handler classes, which must implement an 'execute' method.
+     *
      * @var array
      */
     protected $commands = [
@@ -18,13 +21,18 @@ class CommandHandler
     /**
      * Handles the execution of the given command.
      *
+     * This method checks if the given command exists in the $commands array, 
+     * validates that the associated command class exists, and ensures it has an 'execute' method.
+     * If the command is valid, it is executed with the provided arguments. If 'help' is requested,
+     * or the command is not found, the list of available commands is shown.
+     *
      * @param string $command The command name to execute
      * @param array $args The arguments passed to the command
-     * @throws \Exception If the command is not valid or does not have an execute method.
+     * @throws \Exception If the command does not exist or the command class lacks an 'execute' method.
      */
     public function handle(string $command, array $args)
     {
-        // If 'help' is requested or no command is provided, show the command list
+        // If 'help' is requested or no command is provided, display the list of commands
         if ($command === 'help' || !$command) {
             $this->listCommands();
             return;
@@ -39,11 +47,12 @@ class CommandHandler
 
         $commandClass = $this->commands[$command];
 
-        // Check if the class for the command exists
+        // Check if the corresponding class exists for the command
         if (!class_exists($commandClass)) {
             throw new \Exception("Command class {$commandClass} not found.");
         }
 
+        // Instantiate the command class
         $commandInstance = new $commandClass();
 
         // Ensure the command class has an 'execute' method
@@ -51,12 +60,15 @@ class CommandHandler
             throw new \Exception("Command {$command} does not have an execute method.");
         }
 
-        // Execute the command
+        // Execute the command with the provided arguments
         $commandInstance->execute($args);
     }
 
     /**
      * Lists all available commands, including 'help'.
+     *
+     * This method outputs a list of all available commands in the $commands array,
+     * as well as a default 'help' command that provides assistance to the user.
      */
     protected function listCommands()
     {
@@ -65,7 +77,7 @@ class CommandHandler
         // Always include 'help' as an available command
         echo "  - help" . PHP_EOL;
 
-        // List commands from the $commands array
+        // List all commands from the $commands array
         foreach ($this->commands as $cmd => $class) {
             echo "  - {$cmd}" . PHP_EOL;
         }
