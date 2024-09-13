9792ac20e57218ec920c18a0a50c2d16553b0314
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-09-13 13:24:21 +0200

    :speech_balloon: :lipstick: Create 404 view.

---
+++ public/public_views/notfound/index.php
@@ -0,0 +1,19 @@
+<h1 class="mt-6 md:mt-16 font-concert-one text-2xl sm:text-4xl hsm:text-5xl md:text-6xl text-dark-green dark:text-peach tracking-1 mx-auto w-4/5 text-center">
+    Page not found!
+</h1>
+
+<section class="m-auto w-11/12 mt-24">
+    <div class="w-3/5 mx-auto mt-8 text-lg sm:text-xl md:text-2xl text-black dark:text-white !leading-normal">
+        <p>
+            Sorry, the page you are looking for does not exist.
+        </p>
+        <p>
+            It may have been moved or deleted, or you may have mistyped the URL.
+        </p>
+        <div class="flex mt-8 text-sm sm:text-lg md:text-xl lg:text-2xl">
+            <a href="/" title="Go back to the home page" class="mx-auto border-2 border-dark-green text-dark-green dark:border-peach dark:text-peach pr-4 pl-2 py-1 rounded-md hover:opacity-60 hover:cursor-pointer transition-opacity">
+                Go back to the home page
+            </a>
+        </div>
+    </div>
+</section>
