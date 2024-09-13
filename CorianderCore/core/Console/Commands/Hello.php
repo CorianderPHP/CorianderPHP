f0bcb95c12f88e1f7d1667be4eceec202fee14a8
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-09-13 17:41:11 +0200

    :bulb: Update comments.

--- CorianderCore/core/Console/Commands/Hello.php
+++ CorianderCore/core/Console/Commands/Hello.php
@@ -5,12 +5,15 @@ namespace CorianderCore\Console\Commands;
 class Hello
 {
     /**
-     * Executes the hello command.
+     * Executes the 'hello' command.
      * 
-     * This command outputs a friendly greeting message to the user when invoked.
+     * This method outputs a friendly greeting message to the user when the
+     * 'hello' command is invoked through the CommandHandler. It serves as 
+     * a basic example of a command that can be executed in the console.
      */
     public function execute()
     {
         echo "Hi! I'm Coriander, how can I help you?" . PHP_EOL;
     }
 }
+
