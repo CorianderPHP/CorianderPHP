1e2d1d3fef2f71a23247105986c06745df608ac4
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-09-15 23:08:58 +0200

    :wrench: Create database config template for MySQL.

---
+++ CorianderCore/core/Console/Commands/Database/MySQL/templates/database.php
@@ -0,0 +1,8 @@
+<?php
+// MySQL Configuration
+define('DB_TYPE', 'mysql');
+
+define('DB_HOST', '{{DB_HOST}}'); // Hostname for MySQL server
+define('DB_NAME', '{{DB_NAME}}'); // Database name
+define('DB_USER', '{{DB_USER}}'); // Database username
+define('DB_PASSWORD', '{{DB_PASSWORD}}'); // Database password
