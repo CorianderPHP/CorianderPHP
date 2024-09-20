c3703cc8c2cc035e1907c3df63bc54192d35c07a
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-09-20 17:49:14 +0200

    :wrench: Update variable name meta to metadata.

--- CorianderCore/core/Console/Commands/Make/View/templates/metadata.php
+++ CorianderCore/core/Console/Commands/Make/View/templates/metadata.php
@@ -1,7 +1,7 @@
 <?php
 // Custom metadata for the {{viewName}} page
 $lang = 'en';
-$meta = '
+$metadata = '
 <title>{{viewName}} page</title>
 <meta name="description" content="This is the {{viewName}} page description.">
 ';
