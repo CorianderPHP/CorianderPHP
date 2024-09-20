83b24f07175154c916e6267222b4232ce16527e4
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-09-20 17:49:22 +0200

    :recycle: Update variable name meta to metadata.

--- public/public_views/home/metadata.php
+++ public/public_views/home/metadata.php
@@ -1,7 +1,7 @@
 <?php
 // Custom metadata for the home page
 $lang = 'en';
-$meta = '
+$metadata = '
 <title>home page</title>
 <meta name="description" content="This is the home page description.">
 ';
