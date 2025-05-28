5c1ccee09397fc3e14e654a8bfa2b5f80eee2e40
Dancuo-Lohan <dancuo.lohan@gmail.com>
2025-05-28 22:56:39 +0200

    ✨ Create NameFormatter.

---
+++ CorianderCore/core/Router/NameFormatter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CorianderCore\Router;
+
+/**
+ * Utility class for formatting controller and route names.
+ *
+ * Converts URI segments to PascalCase to match controller class naming conventions.
+ *
+ * @package CorianderCore\Router
+ */
+class NameFormatter
+{
+    /**
+     * Converts a string like 'user-profile' or 'user_profile' to 'UserProfile'.
+     *
+     * @param string $name The raw controller or route segment.
+     * @return string PascalCase formatted string.
+     */
+    public static function toPascalCase(string $name): string
+    {
+        return str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $name)));
+    }
+}
