1f486ad9fd883a8484894cef818393e6d05e9f46
Dancuo-Lohan <dancuo.lohan@gmail.com>
2025-05-30 16:31:23 +0200

    ♻️ Api routes can now have sub routes.

--- CorianderCore/core/Router/ApiControllerHandler.php
+++ CorianderCore/core/Router/ApiControllerHandler.php
@@ -3,7 +3,7 @@
 namespace CorianderCore\Router;
 
 /**
- * Handles dispatching to API controllers based on RESTful methods.
+ * Handles dispatching to API controllers based on RESTful methods and subpaths.
  */
 class ApiControllerHandler
 {
@@ -13,8 +13,6 @@ class ApiControllerHandler
         array_shift($segments); // Remove 'api'
 
         $controllerName = $this->formatControllerName($segments[0] ?? '');
-        $params = array_slice($segments, 1);
-
         $controllerClass = 'ApiControllers\\' . $controllerName . 'Controller';
 
         if (!class_exists($controllerClass)) {
@@ -22,7 +20,16 @@ class ApiControllerHandler
         }
 
         $controller = new $controllerClass();
+
+        // Build action method name
         $action = strtolower($method);
+        if (isset($segments[1]) && $segments[1] !== '') {
+            $subAction = strtolower(str_replace('-', '_', $segments[1]));
+            $action .= '_' . $subAction;
+        }
+
+        // Remaining segments are parameters
+        $params = array_slice($segments, 2);
 
         if (!method_exists($controller, $action)) {
             return false;
